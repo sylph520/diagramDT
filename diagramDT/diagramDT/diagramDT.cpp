b846d90c6c9e435be1fd9dc0caa7706b22883700
sylph520 <pallasjack@outlook.com>
2016-02-19 16:27:26 +0800

    a general description of the task

--- diagramDT/diagramDT/diagramDT.cpp
+++ diagramDT/diagramDT/diagramDT.cpp
@@ -1,11 +1,27 @@
 // diagramDT.cpp : Defines the entry point for the console application.
 //
-
+/*
+This project is meant for the geometry element detectation task with a input diagram picture
+*/
 #include "stdafx.h"
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	/*** Start stage ***/
+	
+	// get the input diagram picture to be the original image
+
+	
+	/*** Image Processing and geo-primitives detection stage ***/
+
+	/** first start image processing part **/
+	// image bianarizing for next processing operation
+	// image labelling with binarized image
+	// image segmenting, find the geometry diagram region and associating labelling letter region
+	
+	/** now begin primitive detection part **/
+
 	return 0;
 }
 
