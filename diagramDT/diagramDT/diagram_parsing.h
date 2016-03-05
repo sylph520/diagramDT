a11d7249ee1129971e51aa43d5e1fa68c400e866
sylph520 <pallasjack@outlook.com>
2016-03-05 20:37:43 +0800

    revision

--- diagramDT/diagramDT/diagram_parsing.h
+++ diagramDT/diagramDT/diagram_parsing.h
@@ -4,4 +4,4 @@ struct spe_point
 	Vec2i pt_location;
 	vector<string> pt_properties;
 };
-void parse_primitive(Mat& color, Mat& colorgeo, Mat& bw, Mat& nonC_geo, vector<Vec3f>& circles, vector<Vec4i>& lines, vector<spe_point>& points);
+void parse_primitive(Mat& color, Mat& graygeo, Mat& bw, vector<Vec3f>& circles, vector<Vec4i>& lines, vector<spe_point>& points);
